976ccfd9ff85d2d13e36f390277a09885088e191
Wes MacDonald <wrmacdonald2016@gmail.com>
2024-03-14 11:35:47 -0700

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Wes MacDonald on 3/14/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
