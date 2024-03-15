423490de9e20887ffe05c190fa12d08b482ba3f0
Wes MacDonald <wrmacdonald2016@gmail.com>
2024-03-15 10:32:47 -0700

    add README & day 1 code

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,14 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 0.0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button("Tap Me") {
+            withAnimation(.smooth) {
+                animationAmount += 80
+            }
         }
-        .padding()
+        .padding(40)
+        .background(.red)
+        .foregroundStyle(.white)
+        .clipShape(.circle)
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0.0, y: 0.0, z: 1.0))
+        // animation with binding animation:
+//        return VStack {
+//            Stepper("Scale amount", value: $animationAmount.animation(), in: 1...10)
+//            
+//            Spacer()
+//            
+//            Button("Tap Me") {
+//                animationAmount += 1
+//            }
+//            .padding(40)
+//            .background(.red)
+//            .foregroundStyle(.white)
+//            .clipShape(.circle)
+//            .scaleEffect(animationAmount)
+//        }
+        
+        // animated button with implicit animations:
+//        Button("Tap Me!") {
+////            animationAmount += 1
+//        }
+//        .padding(50)
+//        .background(.red)
+//        .foregroundStyle(.white)
+//        .clipShape(.circle)
+//        .overlay(
+//            Circle()
+//                .stroke(.red)
+//                .scaleEffect(animationAmount)
+//                .opacity(2 - animationAmount)
+//                .animation(
+//                    .easeOut(duration: 1)
+//                        .repeatForever(autoreverses: false),
+//                    value: animationAmount
+//                )
+//        )
+//        .onAppear {
+//            animationAmount = 2
+//        }
     }
 }
 
---
+++ README.md
@@ -0,0 +1,28 @@
+<div align="left">
+  <h1>Animations</h1>
+  <samp>Project 6 - 100 Days of SwiftUI class</samp>
+  <br/>
+
+  <sub>
+    Author: <a href="https://github.com/wrmacdonald" target="_blank">Wes MacDonald</a>
+    <br/>
+    <small>Mar 2023</small>
+  </sub>
+</div>
+
+## 📸 Screenshots
+<div align="center">
+
+![Project 6 - Demo](./_Screenshots/Demo.gif)
+
+</div>
+
+## 📝 What I Learned
+- implicit animaitions
+- binding animations
+- withAnimation
+
+
+
+
+
